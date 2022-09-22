5cd57ba145c30b4b78b5758befcd9332d1d2cd01
Anumalasetty Venkata Ramana Rakshith <67448523+ramana-anumala@users.noreply.github.com>
2022-09-22 14:56:25 +0530

    BubbleSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,19 +1,37 @@
 import java.util.Arrays;
 public class BubbleSort {
-	static void bubblesort(int array[]) {
-		int size = array.length;
-		for (int i = 0; i < size - 1; i++)
+
+
+				  // perform the bubble sort
+			static void bubbleSort(int array[]) {
+			int size = array.length;
+				    
+				    // loop to access each array element
+			for (int i = 0; i < size - 1; i++)
+				    
+				      // loop to compare array elements
 			for (int j = 0; j < size - i - 1; j++)
-				if (array[j] > array[j + 1]) {
-					int temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
-				 }
-	}
-	public static void main(String args[]) {      
-		int[] data = { -2, 45, 0, 11, -9 };
-		BubbleSort.bubblesort(data);
-		System.out.println("Sorted Array in Ascending Order:");
-		System.out.println(Arrays.toString(data));
-	}
-} 
+
+				        // compare two adjacent elements
+			if (array[j] > array[j + 1]) {   // change > symbol for descending order
+
+				          // swapping occurs if elements are not in the intended order
+			int temp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = temp;
+				        }
+				  }
+
+			public static void main(String args[]) {
+				      
+			int[] data = { 1,2,3,4,5 };
+				    
+				    // call method using class name
+			BubbleSort.bubbleSort(data);
+				    
+			System.out.print("Ascending Order: ");
+			System.out.println(Arrays.toString(data));
+			
+				  }
+			
+				}
