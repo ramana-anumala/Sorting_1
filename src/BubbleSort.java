0c1a06e92f4792c78a52fa580ec188f263a9f175
Anumalasetty Venkata Ramana Rakshith <67448523+ramana-anumala@users.noreply.github.com>
2022-09-22 14:51:57 +0530

    BubbleSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,9 +1,19 @@
-
+import java.util.Arrays;
 public class BubbleSort {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static void bubblesort(int array[]) {
+		int size = array.length;
+		for (int i = 0; i < size - 1; i++)
+			for (int j = 0; j < size - i - 1; j++)
+				if (array[j] > array[j + 1]) {
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				 }
 	}
-
-}
+	public static void main(String args[]) {      
+		int[] data = { -2, 45, 0, 11, -9 };
+		BubbleSort.bubblesort(data);
+		System.out.println("Sorted Array in Ascending Order:");
+		System.out.println(Arrays.toString(data));
+	}
+} 
