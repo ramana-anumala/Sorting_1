0eb8889bfe4024631b45d84bc1564b0146a9975d
Anumalasetty Venkata Ramana Rakshith <67448523+ramana-anumala@users.noreply.github.com>
2022-09-22 15:51:35 +0530

    NEWSORT

---
+++ src/NewSort.java
@@ -0,0 +1,32 @@
+public class NewSort {
+	
+	public static void NewSort(int array[]) {  
+        int n = array.length;  
+        for (int j = 1; j < n; j++) {  
+            int key = array[j];  
+            int i = j-1;  
+            while ( (i > -1) && ( array [i] > key ) ) {  
+                array [i+1] = array [i];  
+                i--;  
+            }  
+            array[i+1] = key;  
+        }  
+    }  
+	
+	void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		NewSort ob = new NewSort();
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+		ob.NewSort(arr);
+		ob.printArray(arr);
+
+	}
+
+}
